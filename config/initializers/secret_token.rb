3a5fa2bb92585565aa253fb7a5fb1eaa7a01c327
Neo Chen <neoxen@qq.com>
2013-06-13 13:17:52 +0800

    project init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WriscMis::Application.config.secret_token = '89d2e1b51eeb6c5e384071a294b96e96f5b40e8d04d30ab8cd968b3fc126a336425885100f539b5c11fed1f3bf322f78a3a08f458ee511ffce35aade8b49f97a'
